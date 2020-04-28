3a2b142c11113ed6b4509e5f9aa080a2acf412c2
Ruchir Khaitan <ruchir0001@gmail.com>
2020-04-28 15:57:51 -0400

    Add serialize batch to binary buffer, dump everything to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+//extern crate abomonation;
 extern crate differential_dataflow;
 extern crate timely;
 
 use crate::differential_dataflow::input::Input;
-use crate::differential_dataflow::operators::arrange::Arrange;
+use crate::differential_dataflow::operators::arrange::{ArrangeByKey, ArrangeBySelf};
 use crate::differential_dataflow::operators::JoinCore;
-use differential_dataflow::trace::implementations::ord::OrdValSpineAbom as DefaultValTrace;
+use abomonation::abomonated::Abomonated;
+use abomonation::encode;
+//use differential_dataflow::trace::implementations::ord::OrdValSpineAbom as DefaultValTrace;
+use timely::dataflow::channels::pact::Pipeline;
+use timely::dataflow::operators::generic::operator::Operator;
+
+//
+// Open questions
+// 1. Why does the batch have an upper of []?
+// 2. How to write down the batch in a sink in a format we can read back in?
+// 3. How do we read the batch data back in
+//  a. it seems like at least in this toy we could interleave a timely source with some control flow
+//  but its a scattered thought
+
 //use abomonation::{encode, decode};
 
 fn main() {
@@ -16,9 +30,10 @@ fn main() {
             // Specifically: we get back a handle to throw stuff in and
             // the actual data
             let (handle, manages) = scope.new_collection();
+            manages.inspect(|x| println!("manages: {:?}", x));
             let reverse = manages.map(|(manager, employee)| (employee, manager));
-            let manages = manages.arrange::<DefaultValTrace<_, _, _, _, u32>>();
-            let reverse = reverse.arrange::<DefaultValTrace<_, _, _, _, u32>>();
+            let manages = manages.arrange_by_key();
+            let reverse = reverse.arrange_by_key();
 
             //let reverse = reverse.arrange_by_key();
 
@@ -27,6 +42,22 @@ fn main() {
                 .join_core(&reverse, |m2, m1, p| Some((*m2, *m1, *p)))
                 .inspect(|x| println!("{:?}", x));
 
+            //lets just print the stream of batches to stdout
+            manages
+                .stream
+                .sink(Pipeline, "StdOutBatchPrinter", |input| {
+                    while let Some((time, data)) = input.next() {
+                        for d in data.iter() {
+                            //let local = (**d).clone();
+                            let mut bytes = Vec::new();
+                            unsafe {
+                                encode(&(**d), &mut bytes);
+                            }
+                            println!("{:?}", d);
+                            println!("{:?}", bytes);
+                        }
+                    }
+                });
             // return the handle so other non-dataflow code can feed us data
             handle
         });
@@ -39,6 +70,7 @@ fn main() {
         for person in 0..size {
             input.insert((person / 2, person));
         }
+        input.advance_to(1);
     })
     .expect("Computation terminated abnormally");
 }
